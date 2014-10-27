dd3eda6cfef59748e560a417423f400ab8516da7
hendrenj <hendrenj@birdsnest.int.colorado.edu>
2014-10-27 13:08:21 -0600

    wrapped filesystem tests with pyfakefs

--- tests/test_filestate.py
+++ tests/test_filestate.py
@@ -1,8 +1,9 @@
 import unittest
 import src.filestate
 import os
+from pyfakefs import fake_filesystem_unittest
 
-class TestFileState(unittest.TestCase):
+class TestFileState(fake_filesystem_unittest.TestCase):
 
     def getFileStat(self, path):
         stat  = os.lstat(path)
@@ -19,12 +20,18 @@ class TestFileState(unittest.TestCase):
             os.makedirs(basedir)
         open(path, 'a').close()
 
+    def setUp(self):
+        self.setUpPyfakefs()
+
+    def tearDown(self):
+        self.tearDownPyfakefs()
+
     def testEmptyFileState(self):
         """
         The permissions should stay the same after calling achieveState()
         on an empty FileState object.
         """
-        tmpf = './tmp/foo'
+        tmpf = '/tmp/foo'
         self.createEmptyFile(tmpf)
         oldstat = self.getFileStat(tmpf)
         fs = src.filestate.FileState()
