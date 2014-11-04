2977a3d89fb98c913a7aca967e2982b37db45adb
hendrenj <hendrenj@birdsnest.int.colorado.edu>
2014-11-04 12:24:18 -0700

    switched chmod/chown functionality from module class to module function; updated test logic while I was at it

--- src/filestate.py
+++ src/filestate.py
@@ -2,6 +2,41 @@ import os
 import pwd
 import grp
 
+def _getUid(user):
+    try:
+        result = pwd.getpwnam(user).pw_uid
+    except (KeyError, TypeError):
+        result = -1
+    return result
+
+def _getGid(group):
+    try:
+        result = grp.getgrnam(group).gr_gid
+    except (KeyError, TypeError):
+        result = -1
+    return result
+
+def update(path,
+           user=None,
+           group=None,
+           add_mode=0,
+           rem_mode=0,
+           set_mode=0):
+    uid = _getUid(user)
+    gid = _getGid(group)
+    stat = os.stat(path)
+    old_mode = stat.st_mode
+    if set_mode:
+        new_mode = set_mode
+    else:
+        new_mode = old_mode | add_mode
+        new_mode = new_mode ^ (rem_mode & new_mode)
+    if ( uid != -1 or gid != -1 ) \
+        and ( uid != stat.st_uid or gid != stat.st_gid ):
+        os.chown(path, uid, gid)
+    if new_mode != old_mode:
+        os.chmod(path, new_mode)
+
 class FileState():
 
     def __init__(self,
--- tests/test_filestate.py
+++ tests/test_filestate.py
@@ -14,22 +14,6 @@ class TestFileState(unittest.TestCase):
     def tearDown(self):
         pass
 
-    def testEmptyFileState(self, mock_grp, mock_pwd, mock_os):
-        # The permissions should stay the same after calling achieveState()
-        # on an empty FileState object.
-
-        fn = "foobar"
-        stat = mock_os.stat.return_value
-        stat.st_mode = 0644
-
-        fs = src.filestate.FileState()
-        fs.achieveState(fn)
-
-        self.assertFalse(mock_os.chown.called,
-            "chown called though file mode will not change")
-        self.assertFalse(mock_os.chmod.called,
-            "chmod called though file mode will not change")
-
     def testChown(self, mock_grp, mock_pwd, mock_os):
         # Chown should only be called when the uid/gid will actually change.
         #
@@ -48,63 +32,94 @@ class TestFileState(unittest.TestCase):
         # map names to uids/gids using mock to spoof results
         uid_map = {user1: uid1, user2: uid2}
         def get_uid(*args):
-            return mock.MagicMock(pw_uid=uid_map[args[0]])
+            user = args[0]
+            if not user: raise TypeError
+            if user not in uid_map: raise KeyError
+            return mock.MagicMock(pw_uid=uid_map[user])
 
         gid_map = {group1: gid1, group2: gid2}
         def get_gid(*args):
-            return mock.MagicMock(gr_gid=gid_map[args[0]])
+            group = args[0]
+            if not group: raise TypeError
+            if group not in gid_map: raise KeyError
+            return mock.MagicMock(gr_gid=gid_map[group])
 
         mock_pwd.getpwnam = mock.MagicMock(side_effect=get_uid)
         mock_grp.getgrnam = mock.MagicMock(side_effect=get_gid)
 
         call_list = []
 
+        # spoof file mode
+        stat = mock_os.stat.return_value
+        stat.st_mode = 0644
+
+        # test update() with no arguments; should do nothing
+        src.filestate.update(fn)
+        self.assertFalse(mock_os.chown.called,
+            "chown called though file mode will not change")
+
+        # test update() with variety of user/group arguments
         def run_filestate_chown(olduser, oldgroup, newuser, newgroup):
-            stat = mock_os.stat.return_value
-            stat.st_mode = 0644
             stat.st_uid = uid_map[olduser]
             stat.st_gid = gid_map[oldgroup]
-            fs = src.filestate.FileState(owner=newuser, group=newgroup)
-            fs.achieveState(fn)
+            src.filestate.update(fn, user=newuser, group=newgroup)
             if olduser != newuser or oldgroup != newgroup:
                 call_list.append(mock.call.os.chown(fn,
                     uid_map[newuser], gid_map[newgroup]))
             self.assertEqual(mock_os.chown.mock_calls, call_list,
                 "chown operation failed")
 
-        # do not change user or group
         run_filestate_chown(user1, group1, user1, group1)
-        # only change user
         run_filestate_chown(user1, group1, user2, group1)
-        # only change group
-        run_filestate_chown(user2, group1, user2, group2)
-        # change both user and group
-        run_filestate_chown(user2, group2, user1, group1)
+        run_filestate_chown(user1, group1, user1, group2)
+        run_filestate_chown(user1, group1, user2, group2)
 
     def testChmod(self, mock_grp, mock_pwd, mock_os):
         # Chmod should only be called if the mode will actually change.
 
         fn = "/path/to/test/file"
-        call_list = []
 
-        def run_filestate_chmod(mode, add_perms, remove_perms, ignore_perms, 
+        # spoof file mode
+        stat = mock_os.stat.return_value
+        stat.st_mode = 0644
+
+        # test update() with no arguments; should do nothing
+        src.filestate.update(fn)
+        self.assertFalse(mock_os.chmod.called,
+            "chmod called though file mode will not change")
+
+        def run_filestate_chmod(mode, add_mode, rem_mode, set_mode, 
                 expected_mode):
+            call_list = []
+
             stat = mock_os.stat.return_value
             stat.st_mode = mode
-            fs = src.filestate.FileState(add_perms=add_perms,
-                remove_perms=remove_perms, ignore_perms=ignore_perms)
-            fs.achieveState(fn)
+
+            # reset mock_calls so we can accurately tell what was called
+            mock_os.chmod.mock_calls = []
+
+            src.filestate.update(fn, add_mode=add_mode,
+                rem_mode=rem_mode, set_mode=set_mode)
             if mode != expected_mode:
                 call_list.append(mock.call.os.chmod(fn, expected_mode))
-            self.assertEqual(mock_os.chmod.mock_calls, call_list,
-                "chmod operation failed")
 
-        run_filestate_chmod(0644, 0, 0, 0, 0644)
-        run_filestate_chmod(0644, 020, 0, 0, 0664)
-        run_filestate_chmod(0644, 0, 04, 0, 0640)
-        run_filestate_chmod(0644, 0, 0644, 0, 0)
-        run_filestate_chmod(0644, 0131, 0020, 0, 0755)
-        run_filestate_chmod(0644, 0131, 0020, 0777, 0644)
-        run_filestate_chmod(0755, 02020, 0, 0, 02775)
-        run_filestate_chmod(0755, 02775, 0, 0, 02775)
-        run_filestate_chmod(0755, 0, 0777, 0, 0)
+            if mock_os.chmod.mock_calls:
+                actual_mode = mock_os.chmod.mock_calls[-1][1][1]
+            else:
+                actual_mode = mode
+            err_msg = "chmod operation failed: start mode: %o, add mode: %o, \
+remove mode: %o, set mode: %o, expected result: %o, actual result: %o" \
+                % (mode, add_mode, rem_mode, set_mode, expected_mode, \
+                    actual_mode)
+            self.assertEqual(mock_os.chmod.mock_calls, call_list, err_msg)
+
+        run_filestate_chmod(0644,  0,     0,     0,     0644 )
+        run_filestate_chmod(0644,  020,   0,     0,     0664 )
+        run_filestate_chmod(0644,  0,     04,    0,     0640 )
+        run_filestate_chmod(0644,  0,     0644,  0,     0    )
+        run_filestate_chmod(0644,  0131,  0020,  0,     0755 )
+        run_filestate_chmod(0644,  0,     0,     0600,  0600 )
+        run_filestate_chmod(0644,  0131,  0020,  0777,  0777 )
+        run_filestate_chmod(0755,  02020, 0,     0,     02775)
+        run_filestate_chmod(0755,  02775, 0,     0,     02775)
+        run_filestate_chmod(0755,  0,     0777,  0,     0    )
